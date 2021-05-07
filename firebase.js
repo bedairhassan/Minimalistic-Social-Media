4821671336fcd953a3bdf24607d9099976f0b6c6
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2021-05-07 08:16:30 +0200

    firebase.js // npm i --save firebase

--- firebase.js
+++ firebase.js
@@ -1,19 +1,13 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.5.0/firebase-app.js"></script>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyA2cWJmERSPGPixbUB1QSjkZKkcAXSiapE",
     authDomain: "facebook-9cc04.firebaseapp.com",
     projectId: "facebook-9cc04",
     storageBucket: "facebook-9cc04.appspot.com",
     messagingSenderId: "542807902668",
     appId: "1:542807902668:web:cd2a0d2b9b602ee3243569"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore()
