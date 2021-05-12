17b883d47ea30c72de6849cda2218d8bdf41d783
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2021-05-12 18:13:29 +0200

    alert('SIGNEDIN')

--- src/visuals/loginnpage/signin_page/SignIn.svelte
+++ src/visuals/loginnpage/signin_page/SignIn.svelte
@@ -1,12 +1,42 @@
 <script>
+import { db } from "../../../JS/firebase";
+
+
+
+// db
 
 let username,password;
+
+
+const fetch = ()=>{
+    db.collection("sign").onSnapshot((snap) => {
+
+        snap = snap.docs
+        console.log(snap)
+
+        snap = snap.map(item=>item.data())
+        console.log(snap)
+
+        snap = snap.filter(item=>item.username===username)
+        console.log(snap)
+
+        if(snap.length>0){
+            alert('SIGNEDIN')
+        }
+    });
+}
+
+const SignIn = ()=>{
+    const condition = fetch()
+}
+
 </script>
 
 <h1>SignIn</h1>
 
 <input type="text" placeholder="enter your username"  bind:value={username}/>
 <input type="password" placeholder="enter your password"  bind:value={password}/>
+<button on:click={SignIn}>SignIn</button>
 
 {username}
 {password}
