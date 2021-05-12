7aff5c9914ee9b8b9b82a71237a018844375e3ac
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2021-05-12 07:16:13 +0200

    routing working perfectly

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,33 @@
 <script>
 
+import { Router, Link, Route } from "svelte-routing";
+
 import Posts from './visuals/posts/Posts.svelte'
 import Friends from './visuals/friends/Friends.svelte'
 import CookiePage from './visuals/DEV/Cookie.svelte'
 
+export let url=""
 
 </script>
 
 
-<CookiePage /> 
+<Router url="{url}">
+
+
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="friends">Friends</Link>
+        <Link to="posts">Posts</Link>
+      </nav>
+
+      <div>
+        <Route path="friends" component="{Friends}" />
+        <Route path="posts" component="{Posts}" />
+        <Route path="/"><CookiePage /></Route>
+      </div>
+
+</Router>
+
+<!-- <CookiePage /> 
 <Friends/>
-<Posts/>
+<Posts/> -->
