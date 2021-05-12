6cee60421e6732849129c866e63908b66dcfa0d2
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2021-05-12 18:03:58 +0200

    userInput

--- src/visuals/loginnpage/signin_page/SignIn.svelte
+++ src/visuals/loginnpage/signin_page/SignIn.svelte
@@ -1,7 +1,12 @@
 <script>
 
-
+let username,password;
 </script>
 
 <h1>SignIn</h1>
 
+<input type="text" placeholder="enter your username"  bind:value={username}/>
+<input type="password" placeholder="enter your password"  bind:value={password}/>
+
+{username}
+{password}
