0abece282ec7d9694787211068a7a983dc6db136
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2021-05-12 07:25:03 +0200

    css works

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,46 @@
 <script>
+    import { Router, Link, Route } from "svelte-routing";
 
-import { Router, Link, Route } from "svelte-routing";
+    import Posts from "./visuals/posts/Posts.svelte";
+    import Friends from "./visuals/friends/Friends.svelte";
+    import CookiePage from "./visuals/DEV/Cookie.svelte";
 
-import Posts from './visuals/posts/Posts.svelte'
-import Friends from './visuals/friends/Friends.svelte'
-import CookiePage from './visuals/DEV/Cookie.svelte'
-
-export let url=""
-
+    export let url = "";
 </script>
 
+<link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous"
+/>
 
-<Router url="{url}">
+<Router {url}>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/">
+                    <Link to="/">Home</Link>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">
+                    <Link to="friends">Friends</Link>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/">
+                    <Link to="posts">Posts</Link>
+                </a>
+            </li>
+        </ul>
+    </nav>
 
-
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="friends">Friends</Link>
-        <Link to="posts">Posts</Link>
-      </nav>
-
-      <div>
-        <Route path="friends" component="{Friends}" />
-        <Route path="posts" component="{Posts}" />
+    <div>
+        <Route path="friends" component={Friends} />
+        <Route path="posts" component={Posts} />
         <Route path="/"><CookiePage /></Route>
-      </div>
-
+    </div>
 </Router>
 
 <!-- <CookiePage /> 
