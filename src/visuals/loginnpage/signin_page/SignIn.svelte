e4ad6a6d83e5fe3472839593cd178ce365a8a2c5
hassan bedair <55901623+bedairhassan@users.noreply.github.com>
2021-05-13 10:46:15 +0200

    ✅signup signin

--- src/visuals/loginnpage/signin_page/SignIn.svelte
+++ src/visuals/loginnpage/signin_page/SignIn.svelte
@@ -1,42 +1,82 @@
 <script>
-import { db } from "../../../JS/firebase";
+    import { db } from "../../../JS/firebase";
+    import signedIn from "../../../store/signedIn";
 
+    // db
 
+    let username, password;
 
-// db
+    let status,
+        show = false;
 
-let username,password;
+    const signup_signin = () => {
+        // sign up immediately creates account by pushing data to server
+        db.collection("sign").doc(username).set({
+            username,
+            password,
+        })
 
+        show='1'
 
-const fetch = ()=>{
-    db.collection("sign").onSnapshot((snap) => {
+        // sign in
+        signedIn.update((lastSignedIn) => username);
+    };
 
-        snap = snap.docs
-        console.log(snap)
+    const fetch = () => {
+        db.collection("sign").onSnapshot((snap) => {
+            snap = snap.docs;
+            console.log(snap);
 
-        snap = snap.map(item=>item.data())
-        console.log(snap)
+            snap = snap.map((item) => item.data());
+            console.log(snap);
 
-        snap = snap.filter(item=>item.username===username)
-        console.log(snap)
+            snap = snap.filter(
+                (item) =>
+                    item.username === username && item.password === password
+            );
+            console.log(snap);
 
-        if(snap.length>0){
-            alert('SIGNEDIN')
-        }
-    });
-}
+            if (snap.length > 0) {
+                alert("SIGNEDIN");
+                // enter your code...
 
-const SignIn = ()=>{
-    const condition = fetch()
-}
+                // update currentSignedIn
 
+                // import signedIn from "../../store/signedIn";
+                signedIn.update((lastSignedIn) => username);
+            } else {
+                // signed up ! !! ! ! !
+                // alert("SIGNEDUP");
+                status = "Do you want to create new account? ";
+                show = '0';
+                // enter your code...
+                // update currentSignedIn
+            }
+
+            // done with if and else?
+            // make routing look better
+        });
+    };
 </script>
 
 <h1>SignIn</h1>
 
-<input type="text" placeholder="enter your username"  bind:value={username}/>
-<input type="password" placeholder="enter your password"  bind:value={password}/>
-<button on:click={SignIn}>SignIn</button>
+<input type="text" placeholder="enter your username" bind:value={username} />
+<input
+    type="password"
+    placeholder="enter your password"
+    bind:value={password}
+/>
+<button on:click={fetch}>SignIn</button>
 
-{username}
-{password}
+<!-- {username}
+{password} -->
+
+{#if show==='0'}
+    {status}
+    <button on:click={signup_signin}>Sign Up & Sign In</button>
+{:else if show==='1'}
+    Signing up.. Signing in.. Please standby
+{/if}
+
+<!-- TODO: if clicks button, show user created?!?!?!?  -->
